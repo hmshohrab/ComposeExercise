797263c9281865ef54535e7cada79bcede6bc0ba
hmshohrab <hmshohrab20@gmail.com>
2022-03-08 23:10:46 +0600

    Initial commit

---
+++ app/src/main/java/com/hmshohrab/composeexercise/MainActivity.kt
@@ -0,0 +1,123 @@
+package com.hmshohrab.composeexercise
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hmshohrab.composeexercise.ui.theme.ComposeExerciseTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeExerciseTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Card(Modifier.fillMaxWidth()) {
+        // Add padding around our message
+        Row(modifier = Modifier.padding(all = 12.dp)) {
+            Image(
+                painter = painterResource(R.drawable.profile_picture),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    // Set image size to 40 dp
+                    .size(40.dp)
+                    // Clip image to be shaped as a circle
+                    .clip(CircleShape)
+            )
+
+            // Add a horizontal space between the image and the column
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column {
+                Text(text = msg.author)
+                // Add a vertical space between the author and message texts
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = msg.body)
+            }
+        }
+    }
+}
+
+@Composable
+fun NewCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard(
+        msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+    )
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeExerciseTheme {
+        Row {
+            Column {
+                NewCard(
+                    msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
+                )
+            }
+        }
+
+    }
+}
