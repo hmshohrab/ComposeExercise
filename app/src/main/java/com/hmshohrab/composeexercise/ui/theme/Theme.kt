9684863cff9c2a04cd757bd7e551c752bbc29487
shohrabdatagridltd <105144172+shohrabdatagridltd@users.noreply.github.com>
2022-11-09 20:57:01 +0600

    Special Consultant design v1.2.1 09-11-2022

--- app/src/main/java/com/hmshohrab/composeexercise/ui/theme/Theme.kt
+++ app/src/main/java/com/hmshohrab/composeexercise/ui/theme/Theme.kt
@@ -5,26 +5,33 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = HARLEQUIN,
     primaryVariant = GREEN,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = HARLEQUIN,
-    primaryVariant = GREEN,
-    secondary = Teal200
-
-    /* Other default colors to override
+    secondary = Teal200,
+    //Other default colors to override
     background = Color.White,
     surface = Color.White,
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
     onSurface = Color.Black,
-    */
+)
+
+private val LightColorPalette = lightColors(
+    primary = HARLEQUIN,
+    primaryVariant = GREEN,
+    secondary = Teal200,
+
+     //Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
 )
 
 @Composable
